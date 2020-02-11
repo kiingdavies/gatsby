32dfa672cb5176a8742de7729488f4e0ca1abdee
kiingdavies <daviesadedeji007@gmail.com>
2020-02-11 12:21:41 +0100

    Header & Navigation Done

--- tango-gatsby/src/components/header.js
+++ tango-gatsby/src/components/header.js
@@ -1,8 +1,60 @@
 import React from 'react';
+import { Link, useStaticQuery, graphql } from 'gatsby';
+
+import Navigation from './Navigation';
+
+import tangoLogo from '../images/tango_logo.svg';
+import { HeaderWrapper } from './styles/HeaderStyles';
 
 const Header = () => {
+    const {
+        site,
+        menu: {
+            edges: [{ node: menu }],
+        },
+    } = useStaticQuery(graphql`
+    query HeaderQuery {
+        site{
+            siteMetadata {
+                title
+            }
+        }
+        menu: allWordpressWpApiMenusMenusItems(
+            filter: { wordpress_id: { eq: 5 }}
+        ){
+            totalCount
+            edges{
+                node{
+                    items {
+                        title
+                        url
+                        wordpress_children{
+                            title
+                            url
+                        }
+                    }
+                    name
+                }
+            }
+        }
+    }`);
 
-    return <div>Header</div>
+    return (
+        <HeaderWrapper>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4"></div>
+                    <Link to="/">
+                        <img src={tangoLogo} alt={site.siteMetadata.title} />
+                    </Link>
+                </div>
+                <div className="col-md-8 menu">
+                    <Navigation menu={menu}/>
+                </div>
+            </div>
+        </HeaderWrapper>
+
+    )
 
 }
 
